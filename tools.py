fcbda9d025cfa410839d2efd5fea006fe3076bb2
joppe <joppe.vos@gmail.com>
2019-11-23 09:03:09 +0100

    adjustments in path structure

--- tools.py
+++ tools.py
@@ -8,14 +8,21 @@ logging.basicConfig(level=logging.DEBUG)
 
 class Exporter:
 
-    def __init__(self, file_name, file_directory, resolution='original', iterations='100:100'):
-        self.file_directory = file_directory
-        self.basename = self.remove_trailing_from_filename(file_name)
-        self.filepath = os.path.join(file_directory, file_name)
+    def __init__(self, file_path, style_image, style_weight=100,
+                 temp_weight=1e3, resolution='original', iterations='50,50'):
+        self.basename = self.remove_trailing_from_filename(file_path)
+        self.filepath = file_path
         self.resolution = resolution
         self.iterations = iterations
-        self.export_folder = 'files/' # location where the videos are located
-        self.image_path = os.path.join(self.export_folder, self.basename) # path to folder where images are extracted
+        self.export_folder = 'files/'  # location where the videos are located
+        self.image_path = os.path.join(self.export_folder, self.basename)  # path to folder where images are extracted
+        self.style_weight = style_weight
+        self.temp_weight = temp_weight
+        self.style_image = style_image
+        self.backend = 'cudnn'
+        self.original_colors = 0
+        self.gpu = 0
+        self.last_frame_index = 1
 
     def make_dir(self, path):
         if not os.path.isdir(path):
@@ -53,26 +60,33 @@ class Exporter:
         os.chdir(f'{home}/Desktop/')
         # todo danger stuff here
         password = 'weesjezelf12'
-        cmd1 = subprocess.Popen(['echo', password], stdout=subprocess.PIPE)
-        cmd2 = subprocess.Popen(['sudo', '-S', 'bash', './test.sh'], stdin=cmd1.stdout, stdout=subprocess.PIPE)
+        # cmd1 = subprocess.Popen(['echo', password], stdout=subprocess.PIPE)
+        # cmd2 = subprocess.Popen(['sudo', '-S', 'bash', './test.sh'], stdin=cmd1.stdout, stdout=subprocess.PIPE)
 
         # restore path
         os.chdir(cwd)
         subprocess.run([f'{home}/torch/install/bin/th', 'artistic_video.lua',
-                        f'-content_pattern', '{self.image_path}/frame_%04d.ppm',
-                        f'-flow_pattern', f'{self.image_path}/flow_{self.resolution}/backward_[%d]_'+'{%d}.flo'])
+                        '-content_pattern', f'{self.image_path}/frame_%04d.ppm',
+                        '-flow_pattern', f'{self.image_path}/flow_{self.resolution}/backward_[%d]_' + '{%d}.flo',
+                        '-style_weight', f'{self.style_weight}',
+                        '-temporal_weight', f'{self.temp_weight}',
+                        '-output_folder', f'inProgress/{self.basename}/{self.basename}_[{self.iterations}]_{self.resolution}',
+                        '-style_image', f'{self.style_image}',
+                        '-backend', f'{self.backend}',
+                        '-num_iterations', f'{self.iterations}',
+                        '-original_colors', f'{self.original_colors}',
+                        '-gpu', f'{self.gpu}',
+                        '-continue_with', f'{self.last_frame_index}',
+                        '-number_format', '%04d'])
 
     def run(self):
         self.export_video_to_images()
         self.make_optical_flow()
-        self.make_optical_flow()
+        # self.start_style_transfer()
 
 
 if __name__ == "__main__":
-    video_file = 'test_video.mp4'
-    E = Exporter(file_directory='files/', file_name=video_file)
-    E.start_style_transfer()
-# run artistic video with arguments
-
-# extras
-# pil display to show input and output
+    E = Exporter(file_path='/home/joppe/projects/style-transfer/input_videos/skater.mp4',
+                 style_image='/home/joppe/projects/style-transfer/boats.png',
+                 resolution='50:50')
+    E.run()
